7cd0c606fe31441282ce7fee8f315922310425a7
OliMar655 <oliverfma2065@outlook.com>
2020-11-22 02:55:51 -0500

    Snacks

--- src/Short.java
+++ src/Short.java
@@ -1,19 +1,21 @@
-import javax.swing.*;
-public class Short extends JFrame{
-    public Short() {
-        JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 5, 1);
-        slider.setMinorTickSpacing(1);
-        slider.setMajorTickSpacing(5);
-        slider.setPaintTicks(true);
-        slider.setPaintLabels(true);
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JList;
 
-        JPanel panel=new JPanel();
-        panel.add(slider);
-        add(panel);
+
+public class Short {
+
+    private void display() {
+        JFrame f = new JFrame("Test");
+        String[] data = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
+        f.add(new JList(data));
+        f.pack();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
     }
-    public static void main(String s[]) {
-        Short frame=new Short();
-        frame.pack();
-        frame.setVisible(true);
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Short()::display);
     }
 }
