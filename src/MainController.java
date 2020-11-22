0084905ea969e7a5a8372a06ff29a9cfe283f1ed
zee-sidd <59626854+zee-sidd@users.noreply.github.com>
2020-11-21 19:56:32 -0500

    Divided Main into MainController to manage events

---
+++ src/MainController.java
@@ -0,0 +1,78 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+// Import necessary java and javafx packages.
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class MainController implements Initializable {
+    @FXML
+    TextField nameTextField;
+
+    @FXML
+    TextField taskNameTextField;
+
+    @FXML
+    ComboBox<String> importanceLevelsBox;
+
+    // Combo box with days for date of birth (from 1 to 31)
+    @FXML
+    ComboBox<String> dayBox;
+
+    // Combo box with months (numbers; 1 to 12) for date of birth
+    @FXML
+    ComboBox<String> monthBox;
+
+    // Combo box with years (numbers; 1 to 12) for date of birth
+    @FXML
+    ComboBox<String> yearBox;
+
+    // These data variables are used to populate the ComboBoxes for days, months, years, and importance level.
+    final ObservableList<String> daysData = FXCollections.observableArrayList();
+    final ObservableList<String> monthData = FXCollections.observableArrayList();
+    final ObservableList<String> yearData = FXCollections.observableArrayList();
+    final ObservableList<String> importanceData = FXCollections.observableArrayList();
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        createComboBoxData();
+        // Set menu items in each box. Use data from Observable list.
+        dayBox.setItems(daysData);
+        monthBox.setItems(monthData);
+        yearBox.setItems(yearData);
+        importanceLevelsBox.setItems(importanceData);
+    }
+
+    @FXML
+    public void schedule() throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("schedule.fxml"));
+        Parent schedule = (Parent) fxmlLoader.load();
+    }
+
+
+    public void createComboBoxData() {
+        // Here we add data to the ObservableList which represents menu items in combo boxes.
+        daysData.addAll(createNumberList(1, 31));
+        monthData.addAll(createNumberList(1, 12));
+        yearData.addAll(createNumberList(2020, 2022));
+        importanceData.addAll("Not Very Important", "Somewhat Important", "Important", "Very Important", "Screwed");
+    }
+
+    //This function creates a list with number randing from parameters start to end.
+    public ObservableList<String> createNumberList(int start, int end) {
+        ObservableList<String> list = FXCollections.observableArrayList();
+        for (int i = start; i <= end; i++) {
+            list.add(Integer.toString(i));
+        }
+        return list;
+    }
+
+}
+
