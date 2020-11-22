811b86ed4d6e34a5568587ad62bec306108dc729
OliMar655 <oliverfma2065@outlook.com>
2020-11-21 21:14:23 -0500

    Snacks

--- src/Short.java
+++ src/Short.java
@@ -1,19 +1,19 @@
-import javax.swing.*;
-import java.awt.*;
 
-class Demo extends JFrame{
-
-    Demo(){
-        JLabel background;
-        setSize(1200, 700);
-        setLayout(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ImageIcon img = new ImageIcon("testing.jpg");
-
-        background = new JLabel("", img, JLabel.CENTER);
-        background.setBounds(0,0,1200,700);
-        add(background);
-
-        setVisible(true);
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+public class  Short{
+    Short(){
+        JFrame f=new JFrame("Button Example");
+        JButton b=new JButton("Play", new ImageIcon("play.png"));
+        b.setBounds(100,100,140, 40);
+        f.add(b);
+        f.setSize(300,400);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        new Short();
     }
 }
